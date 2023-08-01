b0eb3f6266dbe4eaaefd43f81c2f1fa86ef0424a
Max Horstmann <you@example.com>
2023-08-01 17:16:17 +0200

    fixed delete and update function

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const pool = new Pool({
   connectionString: process.env.ELEPHANT_SQL_CONNECTION_STRING,
 });
 
-// add genre
-
-//app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/movies', (req, res) => {
@@ -29,7 +27,7 @@ app.get('/api/movies/:id', (req, res) => {
     .query('SELECT * FROM movies WHERE id=$1;', safeValues)
     .then(({ rowCount, rows }) => {
       if (rowCount === 0) {
-        res.status(404).json({ message: `Movie with ID ${id} not found!` });
+        res.status(404).json({ message: `Movie with id ${id} not found!` });
       } else {
         res.json(rows[0]); //rows is always an array of 1 item
       }
@@ -66,7 +64,7 @@ app.put('/api/movies/:id', (req, res) => {
 });
 app.delete('/api/movies/:id', (req, res) => {
   const { id } = req.params;
-  const safeValues = [title, year, director, genre, image, imdb, metascore, id];
+  const safeValues = [id];
   pool
     .query('DELETE FROM movies WHERE id = $1 RETURNING *;', safeValues)
     .then(({ rows }) => {
@@ -76,7 +74,7 @@ app.delete('/api/movies/:id', (req, res) => {
     .catch((e) => res.status(500).json({ message: e.message }));
 });
 
-app.listen(PORT, () => `SERVER IS UP ON PORT ${PORT}`);
+app.listen(PORT, () => console.log(`SERVER IS UP ON PORT ${PORT}`));
 
 // CREATE TABLE movies (
 //   id serial PRIMARY KEY,
